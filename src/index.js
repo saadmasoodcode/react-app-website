c491fd0d627371cef55c5153b7449f61a5622d82
saadmasoodcode <saadmasoodn@gmail.com>
2024-10-16 22:25:57 +0500

    final 3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="https://saadmasoodcode.github.io/react-app-website">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
